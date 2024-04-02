9db4826a7198877da49f943f74e63f061667eca1
bashybaranaba <bashybaranaba@gmail.com>
2024-04-02 15:02:20 +0300

    feat: add material ui

---
+++ app/theme.ts
@@ -0,0 +1,32 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+import { Chakra_Petch } from "next/font/google";
+
+const chakra_petch = Chakra_Petch({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
+});
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+  },
+  typography: {
+    fontFamily: chakra_petch.style.fontFamily,
+  },
+  components: {
+    MuiAlert: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.severity === "info" && {
+            backgroundColor: "#60a5fa",
+          }),
+        }),
+      },
+    },
+  },
+});
+
+export default theme;
